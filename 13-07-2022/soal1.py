f3f74fa13f7a6711c62f52c4229b67129f6ba050
irzaardian <irzaardian17@gmail.com>
2022-07-13 22:33:06 +0700

    submit soal1.py

--- 13-07-2022/soal1.py
+++ 13-07-2022/soal1.py
@@ -1,1 +1,12 @@
-print ("hello world")
+i = 1
+
+while i <= 100 :
+    if i % 3 == 0 and i % 5 == 0 :
+        print ("FizzBuzz")
+    elif i % 3 == 0:
+        print ("Fizz")
+    elif i % 5 == 0:
+        print ("Buzz")
+    else :
+        print (i)
+    i = i + 1
