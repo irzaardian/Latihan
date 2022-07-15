a5f35decfccd2106e504b184fe9473df26277fda
irzaardian <irzaardian17@gmail.com>
2022-07-15 10:20:48 +0700

    SUbmit soal 4, 5, dan 6

---
+++ 14-07-2022/Soal4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Soal4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int first = 0;
+        int second = 1;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            temp = first + second;
+            first = second;
+            second = temp;
+        }
+    }
+}
