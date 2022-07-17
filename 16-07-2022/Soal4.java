97e200e55c3b9bb3f2237045e518149966954f5c
irzaardian <irzaardian17@gmail.com>
2022-07-17 20:01:28 +0700

    perbaikan soal 4 dan 5

--- 16-07-2022/Soal4.java
+++ 16-07-2022/Soal4.java
@@ -2,24 +2,20 @@ import java.util.ArrayList;
 
 public class Soal4 {
     public static void main(String[] args) {
-        int A[] = { 1, 2, 3, 3, 4 };
+        int A[] = { 1, 1, 2, 3, 5, 3, 4, 4, 4, 5 };
         ArrayList<Integer> move = new ArrayList<Integer>();
 
-        // Asumsi array telah disorting ASC
         for (int i = 0; i < A.length; i++) {
-            int count = 0;
-            for (int j = 0; j < A.length; j++) {
-                if (A[i] == A[j]) {
-                    count++;
+            boolean duplicate = false;
+            for (int j = 0; j < move.size(); j++) {
+                if (A[i] == move.get(j)) {
+                    duplicate = true;
                 }
             }
-            if (count == 1) {
+            if (duplicate == false) {
                 move.add(A[i]);
-            } else {
-                move.add(A[i]);
-                i = i + count - 1;
             }
         }
-        System.out.print(move);
+        System.out.println(move);
     }
 }
--- 16-07-2022/Soal5.java
+++ 16-07-2022/Soal5.java
@@ -1,6 +1,45 @@
 import java.util.Scanner;
 
 public class Soal5 {
+    static String[] sorting(String a[], String b[], String menu) {
+        for (int i = 0; i < b.length; i++) {
+            for (int j = i + 1; j < b.length; j++) {
+                switch (menu) {
+                    case "ASC":
+                        if (b[i].compareTo(b[j]) > 0) {
+                            String temp = b[i];
+                            b[i] = b[j];
+                            b[j] = temp;
+
+                            String temp_buku = a[i];
+                            a[i] = a[j];
+                            a[j] = temp_buku;
+                        }
+                        break;
+                    case "DESC":
+                        if (b[i].compareTo(b[j]) < 0) {
+                            String temp = b[i];
+                            b[i] = b[j];
+                            b[j] = temp;
+
+                            String temp_buku = a[i];
+                            a[i] = a[j];
+                            a[j] = temp_buku;
+                        }
+                    default:
+                        return a;
+                }
+            }
+        }
+        return a;
+    }
+
+    static void print_array(String a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String buku[] = { "The Da Vinci Code", "A Walk To Remember", "Pride and Prejudice", "Harry Potter",
@@ -11,48 +50,7 @@ public class Soal5 {
         for (int i = 0; i < buku_op.length; i++) {
             buku_op[i] = buku[i].replaceAll("\\s", "").toLowerCase();
         }
-        switch (menu) {
-            case "ASC":
-                for (int i = 0; i < buku_op.length; i++) {
-                    for (int j = i + 1; j < buku_op.length; j++) {
-                        if (buku_op[i].compareTo(buku_op[j]) > 0) {
-                            String temp = buku_op[i];
-                            buku_op[i] = buku_op[j];
-                            buku_op[j] = temp;
-
-                            String temp_buku = buku[i];
-                            buku[i] = buku[j];
-                            buku[j] = temp_buku;
-                        }
-                    }
-                }
-                for (int i = 0; i < buku.length; i++) {
-                    System.out.println(buku[i]);
-                }
-                break;
-            case "DESC":
-                for (int i = 0; i < buku_op.length; i++) {
-                    for (int j = i + 1; j < buku_op.length; j++) {
-                        if (buku_op[i].compareTo(buku_op[j]) < 0) {
-                            String temp = buku_op[i];
-                            buku_op[i] = buku_op[j];
-                            buku_op[j] = temp;
-
-                            String temp_buku = buku[i];
-                            buku[i] = buku[j];
-                            buku[j] = temp_buku;
-                        }
-                    }
-                }
-                for (int i = 0; i < buku.length; i++) {
-                    System.out.println(buku[i]);
-                }
-                break;
-            default:
-                for (int i = 0; i < buku.length; i++) {
-                    System.out.println(buku[i]);
-                }
-                break;
-        }
+        String x[] = sorting(buku, buku_op, menu);
+        print_array(x);
     }
 }
