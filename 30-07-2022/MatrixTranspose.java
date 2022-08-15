96fac9f46758d8ad6cd19f21922dc6189d9ee012
irzaardian <irzaardian17@gmail.com>
2022-08-15 12:38:48 +0700

    debug MatrixTranpose

--- 30-07-2022/MatrixTranspose.java
+++ 30-07-2022/MatrixTranspose.java
@@ -1,7 +1,7 @@
 public class MatrixTranspose {
     public static void main(String[] args) {
-        int A[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
-        int B[][] = { { 1, 4 }, { 2, 5, }, { 3, 6 } };
+        int A[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int B[][] = { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } };
 
         int row = A.length;
         int column = A[0].length;
@@ -13,13 +13,16 @@ public class MatrixTranspose {
                 A_transpose[j][i] = A[i][j];
             }
         }
-
-        for (int i = 0; i < column; i++) {
-            for (int j = 0; j < row; j++) {
-                if (A_transpose[i][j] != B[i][j]) {
-                    equal_check = false;
+        if (B.length == A_transpose.length && B[0].length == A_transpose[0].length) {
+            for (int i = 0; i < column; i++) {
+                for (int j = 0; j < row; j++) {
+                    if (A_transpose[i][j] != B[i][j]) {
+                        equal_check = false;
+                    }
                 }
             }
+        } else {
+            equal_check = false;
         }
         System.out.println(equal_check);
     }
